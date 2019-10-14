e7e971593601d6eb23392dadd11c2de43a7720c0
WestViewGold <56317080+WestViewGold@users.noreply.github.com>
2019-10-14 13:36:12 -0700

    Updated button class example
    Added new functionality to the example to test the button class.

--- examples/protoPushButtonClass/protoPushButtonClass.ino
+++ examples/protoPushButtonClass/protoPushButtonClass.ino
@@ -1,17 +1,14 @@
 #include <buttonClass.h>
 
 #define BUTTON_PIN 7
+#define LED_PIN 8
 
-button myButton(BUTTON_PIN);
+button myButton(BUTTON_PIN, LED_PIN);
 
 void setup() {
   // put your setup code here, to run once:
     Serial.begin(9600);
 }
-
-int state = 0;
-int n_press = 0;
-int n_release = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -20,14 +17,39 @@ void loop() {
   if (newstate != state) {
     state = newstate;
     if (state) {
-      n_press++;
+      Serial.println("Button is on");
     } else {
-      n_release++;
-    }
-    Serial.print("Press: ");
-    Serial.print(n_press);
-    Serial.print("  Release: ");
-    Serial.println(n_release);
+    Serial.println("Button is off");
   }
   
+  if (Serial.available() > 0) 
+  {
+    char c = Serial.read();
+    switch c 
+    {
+      case '0':
+        Serial.println("MENU");
+        Serial.println("0: display this menu");
+        Serial.println("1: set state to on");
+        Serial.println("2: set state to off");
+        Serial.println("3: get the current state");
+        Serial.println("4: print the current number of actions");
+        break;
+      case '1':
+        Serial.println("Setting state to on");
+        myButton.setState(true);
+        break;
+      case '2':
+        Serial.println("Setting state to off");
+        mybutton.setState(false);
+        break;
+      case '3':
+        Serial.print("Current state is: ");
+        Serial.println(myButton.getState());
+        break;
+      case '4':
+        Serial.print("Current number of actions is: ");
+        Serial.println(myButton.getNumberOfActions);
+        break;
+    }
 }
