9408b979895cb7a186009a26ffbbe2bac15ca41b
WestViewGold <56317080+WestViewGold@users.noreply.github.com>
2019-10-14 22:20:53 -0700

    Clean up formatting
    Indents were screwed up

--- buttonClass.cpp
+++ buttonClass.cpp
@@ -1,58 +1,55 @@
 #include "buttonClass.h"
 #include "arduino.h"
 
-button::button(int buttonPin, int ledPin)
-{
-  // Set the pin modes for the button and LED pins and save to local variables
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(ledPin, OUTPUT);
-  m_buttonPin = buttonPin;
-  m_ledPin = ledPin;
+button::button(int buttonPin, int ledPin) {
+    // Set the pin modes for the button and LED pins and save to local variables
+    pinMode(buttonPin, INPUT_PULLUP);
+    pinMode(ledPin, OUTPUT);
+    m_buttonPin = buttonPin;
+    m_ledPin = ledPin;
 
-  // Initially, button is off and released.
-  m_state = false;
-  m_pressed = false;
+    // Initially, button is off and released.
+    m_state = false;
+    m_pressed = false;
 
-  // Last action was at t = 0
-  m_timeOfAction = 0;
+    // Last action was at t = 0
+    m_timeOfAction = 0;
 
-  // Haven't done anything yet
-  m_numberOfActions = 0;
+    // Haven't done anything yet
+    m_numberOfActions = 0;
 }
 
-bool button::getState()
-{
-  // Read the button pin
-  bool val = !digitalRead(m_buttonPin);
-//  Serial.println(m_buttonPin);
-//  Serial.println(val);
+bool button::getState() {
+    // Read the button pin
+    bool val = !digitalRead(m_buttonPin);
+    //  Serial.println(m_buttonPin);
+    //  Serial.println(val);
 
-  // How long since the last button action?
-  unsigned long currentTime = millis();
-  unsigned long timeSinceAction = currentTime-m_timeOfAction;
+    // How long since the last button action?
+    unsigned long currentTime = millis();
+    unsigned long timeSinceAction = currentTime-m_timeOfAction;
 
-  // If it has been long enough, then check to see if a press or release action has occurred
-  if (timeSinceAction > 100) {
-    if (val != m_pressed) {
-	  // OK, we have a new action.  Update the time of action and update the pressed/released state
-      m_timeOfAction = currentTime;
-      m_pressed = val;
+    // If it has been long enough, then check to see if a press or release action has occurred
+    if (timeSinceAction > 100) {
+        if (val != m_pressed) {
+	        // OK, we have a new action.  Update the time of action and update the pressed/released state
+            m_timeOfAction = currentTime;
+            m_pressed = val;
 
-      if (m_pressed) {
-        // If this is a button press, then toggle the on/off state and update the LED.
-	m_state = !m_state;
-	digitalWrite(m_ledPin, m_state);
+            if (m_pressed) {
+                // If this is a button press, then toggle the on/off state and update the LED.
+	            m_state = !m_state;
+	            digitalWrite(m_ledPin, m_state);
 
-	// And update the count of number of actions.
-	m_numberOfActions++;
-     }
-   }
-  } 
-  return m_state;
+        	    // And update the count of number of actions.
+	            m_numberOfActions++;
+            }
+        }
+    }  
+    return m_state;
 }
 
-void button::setState(bool state)
-{
+void button::setState(bool state) {
 	// Just overwrite the new state
 	m_state = state;
 
@@ -63,7 +60,6 @@ void button::setState(bool state)
 	m_numberOfActions++;
 }
 
-int button::getNumberOfActions()
-{
+int button::getNumberOfActions() {
 	return m_numberOfActions;
 }
