6b6c6c99f052f5d83202f706cf26deac808609ac
Robert Jacques <robert.jacques@asml.com>
2019-10-17 12:20:44 -0700

    Created lora class
    Initial creation of the Lora class.  Not debugged yet.

---
+++ examples/loraTest/loraTest.ino
@@ -0,0 +1,28 @@
+<include lora.h>
+
+lora myLora;
+
+#define BUFLEN 64;
+char workingBuffer[BUFLEN];
+
+void setup() {
+	Serial.begin(9600);
+	myLora.send("AT+ADDRESS?", workingBuffer, BUFLEN);
+	myLora.send("AT+NETWORKID?", workingBuffer, BUFLEN);
+	myLora.send("AT+PARAMETER=10,7,1,7", workingBuffer, BUFLEN);
+	myLora.send("AT+PARAMETER?",workingBuffer, BUFLEN);
+}
+
+void loop() {
+	if (*myLora.available()) {
+		*myLora.receive(workingBuffer, BUFLEN);
+		Serial.println(workingBuffer);
+	}
+	
+	if (Serial.available() > 0) {
+		String str = "AT+" + Serial.readString();
+		str.remove(str.length() - 1, 1);
+		Serial.println(str);
+		myLora.send(str);
+	}
+}
---
+++ lora.cpp
@@ -0,0 +1,48 @@
+#include "lora.h"
+#include "arduino.h"
+
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+	lora::lora() {
+		Serial.begin(9600);
+	}
+#else
+	lora::lora(int txPin, int rxPin) {
+		*m_port = SofwareSerial(txPin, rxPin);
+		*m_port.begin(9600);
+	}
+#endif
+
+int lora::send(const char* command, char* response, buflen) {
+	#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+		Serial2.print(command);
+		Serial2.print("\r\n");
+		int n = Serial2.readBytesUntil('\n', response, buflen);
+	#else
+		*m_lora.print(command);
+		*m_lora.print("\r\n");
+		int n = *m_lora.readBytesUntil('\n', response, buflen);
+	#endif
+	response[n-1] = 0;
+	return n;
+}
+
+int lora::receive(const char* command, char* response, buflen) {
+	#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+		int n = Serial2.readBytesUntil('\n', response, buflen);
+	#else
+		int n = *m_port.readyBytesUntil('\n', response, buflen);
+	#end
+	response[n-1] = 0;
+	return n;
+}
+
+bool lora::available() {
+	#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+		bool ret = Serial2.available() > 0;
+	#else
+		bool ret = *m_port.available() > 0;
+	#endif
+	
+	return ret;
+}
+	
